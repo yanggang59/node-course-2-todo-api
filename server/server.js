84b7fdde297f67775501b3183a861cdfc973fc9e
yanggang59 <1449381582@qq.com>
2018-11-14 23:13:43 +0800

    Add Patch /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-//const
-var express = require('express');
-var bodyParser= require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser= require('body-parser');
 
 var {ObjectID} = require('mongodb');
 
@@ -83,6 +83,32 @@ app.delete('/todos/:id',(req,res)=>{
       //if doc,send doc with 200
     //error
       //400 with empty body
+});
+
+
+app.patch('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    return res.status(400).send();
+  });
 });
 
 
