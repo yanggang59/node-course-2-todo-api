2404fc286133e5bf7ac19b3975e5b43d472711f2
yanggang59 <1449381582@qq.com>
2018-11-14 18:35:02 +0800

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var bodyParser= require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -34,6 +36,28 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(e);
   });
 });
+
+
+//GET /todos/1231324
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
+});
+
+
 
 
 app.listen(3000,()=>{
